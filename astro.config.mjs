273826d7dc25b4154e99246a83b9766593af7daf
Felipe <felipe@otmizards.com>
2026-01-08 15:50:09 +0000

    Finalize infra and content site04

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+﻿import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+
+export default defineConfig({
+  site: "https://site04.360winspt.com",
+  integrations: [sitemap()],
+});
